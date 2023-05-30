2227be0e1578281ce8560d0797d052401554c23d
Naohide Sano <namachapanda2005@gmail.com>
2023-05-30 15:30:09 +0900

    refactor directories

--- src/main/java/vavi/speech/sandbox/chatgpt/ChatGPT2.java
+++ src/main/java/vavi/speech/sandbox/chatgpt/ChatGPT2.java
@@ -4,33 +4,34 @@
  * Programmed by Naohide Sano
  */
 
-package vavi.speech.sandbox;
+package vavi.speech.sandbox.chatgpt;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
 import javax.speech.Engine;
 import javax.speech.EngineManager;
 import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerMode;
 import javax.speech.synthesis.Voice;
 
-import com.intellijava.core.controller.RemoteLanguageModel;
-import com.intellijava.core.model.input.LanguageModelInput;
+import gg.acai.chatgpt.ChatGPT;
+import gg.acai.chatgpt.Conversation;
+import gg.acai.chatgpt.exception.ParsedExceptionEntry;
 import vavi.speech.modifier.yakuwarigo.YakuwarigoModifier;
+import vavi.util.Debug;
 import vavi.util.properties.annotation.Property;
 import vavi.util.properties.annotation.PropsEntity;
 
 
 /**
- * ChatGPT1.
+ * ChatGPT2.
  *
  * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
  * @version 0.00 2023-01-23 nsano initial version <br>
  */
 @PropsEntity(url = "file:local.properties")
-public class ChatGPT1 {
+public class ChatGPT2 {
 
     @Property(name = "openai.key")
     String apiKey;
@@ -38,11 +39,14 @@ public class ChatGPT1 {
     @Property(name = "chatgpt.model")
     String chatGptModel;
 
+    @Property(name = "user.agent")
+    String userAgent;
+
     /**
      * @param args
      */
     public static void main(String[] args) throws Exception {
-        ChatGPT1 app = new ChatGPT1();
+        ChatGPT2 app = new ChatGPT2();
         PropsEntity.Util.bind(app);
         app.exec();
     }
@@ -64,32 +68,46 @@ public class ChatGPT1 {
         synthesizer.getSynthesizerProperties().setVolume(5);
 
         // yakuwarigo
-        YakuwarigoModifier.ConvertOption option = new YakuwarigoModifier.ConvertOption();
+        var option = new YakuwarigoModifier.ConvertOption();
         option.disableKutenToExclamation = true;
         option.name = "zundamon";
         option.disablePrefix = true;
         option.disableLongNote = true;
         YakuwarigoModifier modifier = new YakuwarigoModifier(option);
 
-        // 1- initiate the remote language chatGptModel
-        RemoteLanguageModel langModel = new RemoteLanguageModel(apiKey, "openai");
+        // cookie
+        MacChromeCookie chromeCookie = new MacChromeCookie();
+        var cookie = chromeCookie.getCookie(".openai.com");
+        String cf_clearance = cookie.get("cf_clearance");
+Debug.println("cf_clearance: " + cf_clearance);
+        String sessionToken = cookie.get("__Secure-next-auth.session-token");
+Debug.println("sessionToken: " + sessionToken);
 
-        // 2- call generateText with any command !
+        // chatgpt
+        ChatGPT chatGpt = ChatGPT.newBuilder()
+                .sessionToken(sessionToken) // required field: get from cookies
+                .cfClearance(cf_clearance) // required to bypass Cloudflare: get from cookies
+                .userAgent(userAgent) // required to bypass Cloudflare: google 'what is my user agent'
+                .addExceptionAttribute(new ParsedExceptionEntry("exception keyword", Exception.class)) // optional: adds an exception attribute
+                .connectTimeout(60L) // optional: specify custom connection timeout limit
+                .readTimeout(30L) // optional: specify custom read timeout limit
+                .writeTimeout(30L) // optional: specify custom write timeout limit
+                .build(); // builds the ChatGPT client
+        Conversation conversation = chatGpt.createConversation();
+
         while (true) {
             System.out.print("Q: ");
             String prompt = new BufferedReader(new InputStreamReader(System.in)).readLine();
             if (prompt.equals("quit")) break;
-            LanguageModelInput langInput = new LanguageModelInput.Builder(prompt)
-                    .setModel(chatGptModel)
-                    .setTemperature(0.5f)
-                    .setMaxTokens(1024).build();
-            String resValue = langModel.generateText(langInput);
-            System.out.println("A:");
-            resValue = modifier.convert(resValue);
-            for (String line : resValue.split("。")) {
-                System.out.println(line);
-                synthesizer.speak(line, null);
-            }
+            conversation.sendMessageAsync(prompt)
+                    .whenComplete((response) -> { // called when the promise is completed with its response
+                        System.out.println("A:");
+                        String text = response.getMessage();
+                        for (String line : text.split("。")) {
+                            System.out.println(line);
+                            synthesizer.speak(line, null);
+                        }
+                    });
         }
 
         synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
--- src/main/java/vavi/speech/sandbox/openai/ChatGPT1.java
+++ src/main/java/vavi/speech/sandbox/openai/ChatGPT1.java
@@ -4,35 +4,33 @@
  * Programmed by Naohide Sano
  */
 
-package vavi.speech.sandbox;
+package vavi.speech.sandbox.openai;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+
 import javax.speech.Engine;
 import javax.speech.EngineManager;
 import javax.speech.synthesis.Synthesizer;
 import javax.speech.synthesis.SynthesizerMode;
 import javax.speech.synthesis.Voice;
 
-import gg.acai.chatgpt.ChatGPT;
-import gg.acai.chatgpt.Conversation;
-import gg.acai.chatgpt.exception.ParsedExceptionEntry;
+import com.intellijava.core.controller.RemoteLanguageModel;
+import com.intellijava.core.model.input.LanguageModelInput;
 import vavi.speech.modifier.yakuwarigo.YakuwarigoModifier;
-import vavi.util.Debug;
-import vavi.util.StringUtil;
 import vavi.util.properties.annotation.Property;
 import vavi.util.properties.annotation.PropsEntity;
 
 
 /**
- * ChatGPT2.
+ * ChatGPT1.
  *
  * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
  * @version 0.00 2023-01-23 nsano initial version <br>
  */
 @PropsEntity(url = "file:local.properties")
-public class ChatGPT2 {
+public class ChatGPT1 {
 
     @Property(name = "openai.key")
     String apiKey;
@@ -40,14 +38,11 @@ public class ChatGPT2 {
     @Property(name = "chatgpt.model")
     String chatGptModel;
 
-    @Property(name = "user.agent")
-    String userAgent;
-
     /**
      * @param args
      */
     public static void main(String[] args) throws Exception {
-        ChatGPT2 app = new ChatGPT2();
+        ChatGPT1 app = new ChatGPT1();
         PropsEntity.Util.bind(app);
         app.exec();
     }
@@ -69,46 +64,32 @@ public class ChatGPT2 {
         synthesizer.getSynthesizerProperties().setVolume(5);
 
         // yakuwarigo
-        var option = new YakuwarigoModifier.ConvertOption();
+        YakuwarigoModifier.ConvertOption option = new YakuwarigoModifier.ConvertOption();
         option.disableKutenToExclamation = true;
         option.name = "zundamon";
         option.disablePrefix = true;
         option.disableLongNote = true;
         YakuwarigoModifier modifier = new YakuwarigoModifier(option);
 
-        // cookie
-        MacChromeCookie chromeCookie = new MacChromeCookie();
-        var cookie = chromeCookie.getCookie(".openai.com");
-        String cf_clearance = cookie.get("cf_clearance");
-Debug.println("cf_clearance: " + cf_clearance);
-        String sessionToken = cookie.get("__Secure-next-auth.session-token");
-Debug.println("sessionToken: " + sessionToken);
+        // 1- initiate the remote language chatGptModel
+        RemoteLanguageModel langModel = new RemoteLanguageModel(apiKey, "openai");
 
-        // chatgpt
-        ChatGPT chatGpt = ChatGPT.newBuilder()
-                .sessionToken(sessionToken) // required field: get from cookies
-                .cfClearance(cf_clearance) // required to bypass Cloudflare: get from cookies
-                .userAgent(userAgent) // required to bypass Cloudflare: google 'what is my user agent'
-                .addExceptionAttribute(new ParsedExceptionEntry("exception keyword", Exception.class)) // optional: adds an exception attribute
-                .connectTimeout(60L) // optional: specify custom connection timeout limit
-                .readTimeout(30L) // optional: specify custom read timeout limit
-                .writeTimeout(30L) // optional: specify custom write timeout limit
-                .build(); // builds the ChatGPT client
-        Conversation conversation = chatGpt.createConversation();
-
+        // 2- call generateText with any command !
         while (true) {
             System.out.print("Q: ");
             String prompt = new BufferedReader(new InputStreamReader(System.in)).readLine();
             if (prompt.equals("quit")) break;
-            conversation.sendMessageAsync(prompt)
-                    .whenComplete((response) -> { // called when the promise is completed with its response
-                        System.out.println("A:");
-                        String text = response.getMessage();
-                        for (String line : text.split("。")) {
-                            System.out.println(line);
-                            synthesizer.speak(line, null);
-                        }
-                    });
+            LanguageModelInput langInput = new LanguageModelInput.Builder(prompt)
+                    .setModel(chatGptModel)
+                    .setTemperature(0.5f)
+                    .setMaxTokens(1024).build();
+            String resValue = langModel.generateText(langInput);
+            System.out.println("A:");
+            resValue = modifier.convert(resValue);
+            for (String line : resValue.split("。")) {
+                System.out.println(line);
+                synthesizer.speak(line, null);
+            }
         }
 
         synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
