67ff2f6747ea2c8ddfa3fa0887249b3eb3a442bf
Naohide Sano <namachapanda2005@gmail.com>
2024-09-08 15:18:05 +0900

    🥺🥺

--- src/test/java/vavi/speech/sandbox/TestCase.java
+++ src/test/java/vavi/speech/sandbox/TestCase.java
@@ -6,7 +6,12 @@
 
 package vavi.speech.sandbox;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
 import vavi.speech.sandbox.bingai.BingAIChat;
 import vavi.speech.sandbox.ollama.Ollama;
 
@@ -17,7 +22,12 @@ import vavi.speech.sandbox.ollama.Ollama;
  * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (nsano)
  * @version 0.00 2023-01-23 nsano initial version <br>
  */
+@EnabledIf("localPropertiesExists")
 class TestCase {
+
+    static boolean localPropertiesExists() {
+        return Files.exists(Paths.get("local.properties"));
+    }
 
     @Test
     void test1() throws Exception {
@@ -25,6 +35,7 @@ class TestCase {
     }
 
     @Test
+    @Disabled("doesn't work on jbr terminal")
     void test2() throws Exception {
         Ollama.main(new String[] {});
     }
